c94b231511e4230a9841cceaeee20a25e0fbdab1
karun arora <arorakarun786@gmail.com>
2018-09-20 15:23:22 +0530

    First commit

---
+++ js/admob.js
@@ -0,0 +1,28 @@
+  function onDeviceReady(){
+ 
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-8445604951136736/1726497870',
+        interstitial: 'ca-app-pub-8445604951136736/7062585032'
+    };
+} 
+
+if(AdMob) AdMob.createBanner( {
+    //isTesting:true, //Remove this Before publishing your app
+    adId:admobid.banner, 
+    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+    autoShow:true} );
+// Note that autoShow is true
+if(window.AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+if(window.AdMob) AdMob.showInterstitial();
+
+
+
+
+  }
+
+ document.addEventListener("deviceready", onDeviceReady, false);
+
+
